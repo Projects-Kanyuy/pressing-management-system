28f7954e6d3df9fd6123ecb3e0353c6dd8bc140d
Rouclec <senatorasonganyi97@gmail.com>
2025-11-13 13:12:37 +0100

    converter lambda wraper to use import instead of require

--- server/lambda.js
+++ server/lambda.js
@@ -1,4 +1,4 @@
-const server = require("./server");
-const serverless = require("@vendia/serverless-express");
+import app from "./server.js";
+import serverless from "@vendia/serverless-express";
 
-exports.handler = serverless({ app: server });
+export const handler = serverless({ app });
