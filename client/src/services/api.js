9bbcf5b4c98ad9907a36b79971527be555e5e984
ndip123 <nojong23@gmail.com>
2025-11-22 08:02:35 +0100

    for change of URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 // client/src/services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_BASE_URL || process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = API_URL = 'https://api.pressmark.site'  || process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 console.log(`[api.js] API requests will be sent to: ${API_URL}`);
 
 const api = axios.create({
