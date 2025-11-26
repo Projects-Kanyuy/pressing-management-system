9b72dfcfbb3173ef85cda2ed0624e739fd010833
ndip123 <nojong23@gmail.com>
2025-11-26 07:13:38 +0100

    for callback URL update

--- server/controllers/publicController.js
+++ server/controllers/publicController.js
@@ -74,7 +74,7 @@ const initiateRegistration = asyncHandler(async (req, res) => {
         
         const transaction_id = `PRESSFLOW-SUB-${pendingUser._id}-${crypto.randomBytes(4).toString('hex')}`;
         pendingUser.signupData.transactionId = transaction_id;
-        const redirectUrl = `${process.env.FRONTEND_URL}/#/verify-payment?transaction_id=${transaction_id}`;
+        const redirectUrl = `${process.env.FRONTEND_URL}/#/verify-payment?transaction_id=${transaction_id}&email=${pendingUser.email}`;
 
 // --- ADD THIS LOG for definitive proof ---
         console.log(`[Payment Flow] Sending this redirect_url to AccountPe: ${redirectUrl}`);
