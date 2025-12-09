3a9143b2147737f7cfc4ce3db9aa60c3ca92a762
ndip123 <nojong23@gmail.com>
2025-12-09 14:00:09 +0100

    for pixel code

--- client/src/components/PixelTracker.js
+++ client/src/components/PixelTracker.js
@@ -1,32 +1,32 @@
-// src/components/PixelTracker.jsx
-
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { initPixel, trackPageView } from '../utils/pixel';
 
 const PixelTracker = () => {
   const location = useLocation();
-  const [isInitialized, setIsInitialized] = useState(false);
+  const pixelId = process.env.REACT_APP_META_PIXEL_ID;
 
-  // Initialize the pixel on the first component mount
   useEffect(() => {
-    if (!process.env.REACT_APP_FACEBOOK_PIXEL_ID) {
-      console.warn("Meta Pixel ID not found in environment variables.");
-      return;
+    if (!pixelId) return;
+
+    if (!window.fbq) {
+      // eslint-disable-next-line no-unused-expressions
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      
+      window.fbq('init', pixelId);
     }
 
-    initPixel();
-    setIsInitialized(true);
-  }, []);
+    window.fbq('track', 'PageView');
 
-  // Track page views on route changes
-  useEffect(() => {
-    if (isInitialized) {
-      trackPageView();
-    }
-  }, [location, isInitialized]);
+  }, [location, pixelId]);
 
-  return null; // This component does not render anything
+  return null;
 };
 
 export default PixelTracker;
